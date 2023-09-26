86830fc13134790e9c2a2f19d05502919580c9f5
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-09-26 15:03:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+}
+
+function updateEmployeeWithKeyAndValue(x, y, z) {
+    const newEmployee = {...x};
+    newEmployee[y] = z;
+    // console.log(Object.keys(employee) + ' with value ' + Object.values(employee) + ' \n new object ' + Object.keys(newEmployee) + ' with value ' + Object.values(newEmployee));
+    // console.log(newEmployee);
+    // console.log(employee);
+    return newEmployee;
+}
+
+updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(x, y, z) {
+    x[y] = z;
+    console.log(x);
+    return x;
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+
+function deleteFromEmployeeByKey(x, y) {
+    const newEmployee = {...x};
+    delete newEmployee[y];
+    return newEmployee;
+}
+
+deleteFromEmployeeByKey(employee, 'streetAddress');
+
+
+function destructivelyDeleteFromEmployeeByKey(x, y) {
+    x = employee;
+    delete x[y];
+    console.log(x);
+    console.log(employee);
+    return x;
+}
+
+let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
